68ebed2a795133d6088b1e83ec0a4659e6ded4d0
Hasty Alvin <quangthang0311@gmail.com>
2024-10-14 05:18:22 +0700

    Dự án Học Typescript với React - Holetex YT

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Count } from "./Count";
+/*1. Tạo type cho prop trong Component 
+2. Type và interface
+3. Tạo type cho function
+4. Type cho hook usestate
+*/
+function App() {
+  const [count, setCount] = useState(0);
+
+  function total (number1: number, number2: number): number {
+    return number1 + number2;
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <Count count={count} setCount={setCount} />
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Count.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+// type Props = {
+//   count: number;
+//   setCount: React.Dispatch<React.SetStateAction<number>>;
+// };
+
+interface IProps {
+  count: number;
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export const Count = ({ count, setCount }: IProps) => {
+  return (
+    <div className="card">
+      <button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
+      <p>
+        Edit <code>src/App.tsx</code> and save to test HMR
+      </p>
+    </div>
+  );
+};
